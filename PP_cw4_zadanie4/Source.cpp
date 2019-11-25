ac8ca1a75726446d804f3783d3791d1d1b7c45d9
Miroslaw Puczynski <miroslaw@puczynski.org>
2019-11-25 07:23:22 +0100

    001

--- PP_cw4_zadanie4/Source.cpp
+++ PP_cw4_zadanie4/Source.cpp
@@ -2,6 +2,26 @@
 
 using namespace std;
 
+double moja_pow(double liczba, int p) {
+	double potega = 1;
+	if (p == 0) {
+		return potega;
+	}
+	else if (p < 0) {
+		for (int i = 0; i < -p; i++) {
+			potega *= potega;
+		}
+		return 1.0 / potega;
+	}
+	else {
+		for (int i = 0; i < p; i++) {
+			potega *= potega;
+		}
+		return potega;
+	}
+}
+
+
 int main() {
 
 }
